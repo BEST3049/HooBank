8665f94854d0ad448bdba8db79145227dc14149b
Dharmeesh N Rahod <dharmeesh3049@gmail.com>
2025-03-04 12:16:20 +0530

    small change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -43,7 +43,7 @@ const Footer = () => (
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
         Copyright Ⓒ 2022 HooBank. All Rights Reserved.<br />
-        Designed and Developed by Dharmeesh Rathod
+        Designed and Developed by Dharmeesh Rathod(Freelancer)
       </p>
 
       <div className="flex flex-row md:mt-0 mt-6">
